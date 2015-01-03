1e4fee65323469f94c5791baf83bb3ad18de88e5
Steve Kinney <hello@stevekinney.net>
2015-01-02 22:24:25 -0700

    Get a basic synth up and running

--- sketch.js
+++ sketch.js
@@ -3,40 +3,58 @@ var width, height, droplets;
 function setup() {
   width = 640;
   height = 480;
-  droplets = [new Droplet()];
   createCanvas(width, height);
+  droplets = [new Droplet()];
   noStroke();
 }
 
 function draw() {
   background(255);
-  fill(95, 160, 191, 50);
   if (droplets.length < 12) {
-    droplets.push(new Droplet());
+    droplets.push(new Droplet({immortal: true}));
   }
   droplets.forEach(function (droplet) {
     droplet.moveDown().draw();
   });
 }
 
+function mouseClicked() {
+  var options = {
+    x: mouseX,
+    y: mouseY,
+    color: color(237, 59, 59, 180)
+  };
+  droplets.push(new Droplet(options));
+}
+
 function Droplet(options) {
   options = options || {};
+  
+  this.immortal = options.immortal || false;
   
   this.height = options.height || 0;
   this.width = options.width || 0;
   
+  this.color = options.color || color(118, 199, 237, 50);
+  
   this.maxHeight = options.maxHeight || 50;
   this.maxWidth = options.maxWidth || 50;
   
-  this.noise = new p5.Oscillator(this.x);
+  this.noise = new p5.Oscillator('sine');
   
-  this.setOrigin();
-  this.makeNoise();
+  this.ressurect(options.x, options.y);
 }
 
 Droplet.prototype.draw = function () {
   this.adjustSize();
+  fill(this.color);
   ellipse(this.x, this.y, this.width, this.height);
+  return this;
+};
+
+Droplet.prototype.setOrigin = function (x, y) {
+  this.x = x || getRandomNumber(this.maxWidth, width - this.maxWidth);
+  this.y = y || getRandomNumber(0, height / 2);
   return this;
 };
 
@@ -59,27 +77,33 @@ Droplet.prototype.biggerize = function () {
 Droplet.prototype.smallerize = function () {
   this.height--; 
   this.width--;
-  if (this.width === 0) { this.setOrigin(); }
-  return this;
-};
-
-Droplet.prototype.setOrigin = function () {
-  this.makeNoise();
-  this.x = getRandomNumber(this.maxWidth, width - this.maxWidth);
-  this.y = getRandomNumber(0, height / 2);
+  if (this.width === 0) {
+    this.kill();
+  }
   return this;
 };
 
 Droplet.prototype.makeNoise = function () {
+  var env = new p5.Env(0.1, 0.9, 0.5, 0, 0.1, 0.1, 1);
+  
+  this.noise.f = this.x + 440;
+  this.noise.amp(env);
   this.noise.start();
-  setTimeout(function () {
-    this.stopNoise();
-  }.bind(this), 100);
+ 
+  env.play();
+  
+  return this;
 };
 
-Droplet.prototype.stopNoise = function () {
-  this.noise.f = this.x + 440;
-  this.noise.stop();
+Droplet.prototype.kill = function () {
+  if (this.immortal) { this.ressurect(); }
+  return this;
+};
+
+Droplet.prototype.ressurect = function (x, y) {
+  this.setOrigin(x, y);
+  if (!this.immortal) { this.makeNoise(); }
+  return this;
 };
 
 function getRandomNumber(min, max) {
